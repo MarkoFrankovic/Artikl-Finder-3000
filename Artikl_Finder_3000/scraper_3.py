1b906770b05800c7a9dceedf0f7bea2aecc3cf39
Marko <marko.frankovic39@gmail.com>
2023-11-20 20:03:02 +0100

    Scraper Tommy

---
+++ Artikl_Finder_3000/scraper_3.py
@@ -0,0 +1,20 @@
+import asyncio
+from bs4 import *
+import requests
+
+def scraper():
+
+    title = "Tommy"
+    url = "https://www.tommy.hr/akcije"
+    html = requests.get(url).text
+    soup = BeautifulSoup(html, 'html.parser')
+
+    for tag in soup.find_all("h3",{"class":"mb-2 text-sm pr-2 font-normal text-gray-900 line-clamp-2 hover:underline cursor-pointer"}):
+        names = tag.text.strip()
+        print(names)
+
+    for tag in soup.find_all("span",{"class":"mt-auto inline-block-block text-sm font-bold text-gray-900"})[::2]:
+        price = tag.text.strip()[:-9].strip()
+        print(price)
+
+scraper()
