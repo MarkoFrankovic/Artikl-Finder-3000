571d02931ed05c122af2e5963c25d60b88c170ea
Marko <marko.frankovic39@gmail.com>
2023-12-18 13:00:34 +0100

    Povezivanje na MongoDB

--- Artikl_Finder_3000/colector.py
+++ Artikl_Finder_3000/colector.py
@@ -1,5 +1,29 @@
 from fastapi import FastAPI
 import aiohttp
 import asyncio
+import pymongo
 
 app = FastAPI()
+
+#myclient = pymongo.MongoClient(
+    #"mongodb+srv://Korisnik:korisnik@databaza.tip3k.mongodb.net/Databaza?retryWrites=true&w=majority")
+
+myclient = pymongo.MongoClient(
+    "mongodb+srv://Marko:marko39@databaza.hhhcb9p.mongodb.net/Databaza?retryWrites=true&w=majority")
+
+#izbor databaze
+mydb = myclient["Databaza"]
+
+Artikli = mydb["Artikli"]
+
+print(Artikli)
+
+
+@app.route('/api/artikli', methods=['POST'])
+def upis_u_bazu():
+   data = request.get_json()
+   print(json_util.dumps(data))
+   mydict = data
+   data["ocjena"] = int(data["ocjena"])
+   Pjesme.insert_one(data)
+   return data
