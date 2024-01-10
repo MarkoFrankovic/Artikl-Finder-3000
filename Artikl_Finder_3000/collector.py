e053bea066e9fbf08ce931fd5cdcf1fe36582009
Marko <marko.frankovic39@gmail.com>
2024-01-10 16:33:13 +0100

    Ispravak collectora

--- Artikl_Finder_3000/collector.py
+++ Artikl_Finder_3000/collector.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
 import pymongo
-import httpx
+import requests
 
 app = FastAPI()
 
@@ -8,16 +8,11 @@ myclient = pymongo.MongoClient("mongodb+srv://Marko:marko39@cluster0.byoifdj.mon
 mydb = myclient["Databaza"]
 Artikli = mydb["Artikli"]
 
-@app.on_event("shutdown")
-def shutdown_event():
-    myclient.close()
-
-async def upis_u_bazu():
-    async with httpx.AsyncClient() as client:
-        response = await client.get("http://localhost:8000/podatci")
-        rezultat = response.json()
-        mydict = rezultat
-        Artikli.insert_many(mydict["Podatci"])
-        return rezultat
+def upis_u_bazu():
+   response = requests.get(f"http://localhost:8000/podatci")
+   rezultat = response.json()
+   mydict = rezultat
+   Artikli.insert_many(mydict["Podatci"])
+   return rezultat
 
 upis_u_bazu()
